05884698d55c6297507111f2f0acdf7cb7ca5989
terry <mingyteng@163.com>
2020-12-06 19:08:20 +0800

    chapter2 linkedlist init, addToTail and removeNode func

--- src/main/java/chapter2/struct/linkedlist/LinkedList.java
+++ src/main/java/chapter2/struct/linkedlist/LinkedList.java
@@ -1,4 +1,39 @@
 package chapter2.struct.linkedlist;
 
 public class LinkedList {
+    private ListNode head;
+
+    public void addToTail(int value) {
+        if (head == null) {
+            head = new ListNode();
+        }
+        ListNode node = head;
+        while (node.next != null) {
+            node = node.next;
+        }
+        node.next = new ListNode(value);
+
+    }
+
+    public void removeNode(int value) {
+        if (head == null) return ;
+        ListNode node = head;
+        while (node.next != null) {
+            if (node.next.val == value) {
+                node.next = node.next.next;
+                break;
+            }
+        }
+    }
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode () { }
+        ListNode(int value) {
+            val = value;
+            next = null;
+        }
+    }
 }
