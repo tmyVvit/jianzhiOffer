c24d3c5631b44be217b0e805eada438357c90865
terry <mingyteng@163.com>
2020-12-06 19:19:50 +0800

    chapter2 linkedlist print and print reverse

--- src/main/java/chapter2/struct/linkedlist/LinkedList.java
+++ src/main/java/chapter2/struct/linkedlist/LinkedList.java
@@ -1,33 +1,62 @@
 package chapter2.struct.linkedlist;
 
-public class LinkedList {
-    private ListNode head;
+import java.util.Stack;
 
-    public void addToTail(int value) {
-        if (head == null) {
-            head = new ListNode();
-        }
+public class LinkedList {
+    private ListNode head = new ListNode();
+
+    public LinkedList addToTail(int value) {
         ListNode node = head;
         while (node.next != null) {
             node = node.next;
         }
         node.next = new ListNode(value);
-
+        return this;
     }
 
-    public void removeNode(int value) {
-        if (head == null) return ;
+    public LinkedList removeNode(int value) {
+        if (head == null) return this;
         ListNode node = head;
         while (node.next != null) {
             if (node.next.val == value) {
                 node.next = node.next.next;
                 break;
             }
+            node = node.next;
         }
+        return this;
+    }
+
+    public String printList() {
+        ListNode node = head;
+        StringBuilder sb = new StringBuilder();
+        while (node.next != null) {
+            sb.append(node.next.val).append(" --> ");
+            node = node.next;
+        }
+        sb.append("null \n");
+        return sb.toString();
+    }
+
+    public String printReverse() {
+        Stack<Integer> stack = new Stack<Integer>();
+        ListNode node = head;
+        while (node.next != null) {
+            stack.push(node.next.val);
+            node = node.next;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.empty()) {
+            if (sb.length() > 0) {
+                sb.append(" --> ");
+            }
+            sb.append(stack.pop());
+        }
+        return sb.toString();
     }
 
 
-    class ListNode {
+    static class ListNode {
         int val;
         ListNode next;
         ListNode () { }
@@ -35,5 +64,13 @@ public class LinkedList {
             val = value;
             next = null;
         }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addToTail(1).addToTail(2).addToTail(3);
+        list.removeNode(2);
+        System.out.println(list.printList());
+        System.out.println(list.printReverse());
     }
 }
